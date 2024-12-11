de43d9701a37a6884d4340c60e28fbc273a444ca
pavan <159290662+paavann@users.noreply.github.com>
2024-12-11 16:56:09 +0530

    CRUD ops using redux

--- src/features/users/userApiSlice.js
+++ src/features/users/userApiSlice.js
@@ -40,6 +40,43 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 }
             }
         }),
+
+        addUsers: builder.mutation({
+            query: initialUserData => ({
+                url: '/users',
+                method: 'POST',
+                body: {
+                    ...initialUserData,
+                }
+            }),
+            invalidatesTags: [
+                { type: 'User', id: "LIST" }
+            ]
+        }),
+
+        updateUsers: builder.mutation({
+            query: initialUserData => ({
+                url: '/users',
+                method: 'PATCH',
+                body: {
+                    ...initialUserData,
+                }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'User', id: arg.id }
+            ]
+        }),
+
+        deleteUser: builder.mutation({
+            query: ({ id }) => ({
+                url: '/users',
+                method: 'DELETE',
+                body: { id }
+            }),
+            invalidatesTags: (result, error, arg) => [
+                { type: 'User', id: arg.id }
+            ]
+        })
     }),
 })
  //?
