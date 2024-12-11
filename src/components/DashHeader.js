e9ed4624be7cf2886974b618a09e727bf6f9a619
pavan <159290662+paavann@users.noreply.github.com>
2024-12-11 12:32:12 +0530

    bug fixes and changes

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -8,7 +8,7 @@ const DashHeader = () => {
   return (
     <header className='dash-header'>
         <div className='dash-header__container'>
-            <Link to='/dash/notes'>
+            <Link to='/dash'>
                 <h1 className='dash-header__title'>idealNotes</h1>
             </Link>
             <nav className='dash-header__nav'>
