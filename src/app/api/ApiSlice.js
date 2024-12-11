670ddf844880ccefbcaaa477969a5d35ac7f832c
pavan <159290662+paavann@users.noreply.github.com>
2024-12-11 12:34:14 +0530

    bug fixes and changes

--- src/app/api/ApiSlice.js
+++ src/app/api/ApiSlice.js
@@ -5,6 +5,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({ //createApi ----> used for creating an api slice for the app(central place where all the api configurations are managed)
     baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000'}), //defines the base url for all the api requests
-    tagTypes: ['Users', 'Notes'], //placeholders for caching data
+    tagTypes: ['User', 'Note'], //placeholders for caching data
     endpoints: builder => ({}) //endpoints where the api operations are mentioned (builder is an object given to us by createApi)
 })
